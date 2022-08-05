176b26a3f5712eab0700e7a3c250606b29df1802
Valeriu Ionut Iocsa <valeriuionutiocsa@gmail.com>
2022-08-05 10:10:42 +0300

    small improvements

--- app/src/main/java/com/utils/log/progress/ProgressIndicators.java
+++ app/src/main/java/com/utils/log/progress/ProgressIndicators.java
@@ -1,7 +1,5 @@
 package com.utils.log.progress;
 
-import com.utils.log.Logger;
-
 public final class ProgressIndicators {
 
 	private static ProgressIndicator instance = ProgressIndicatorNoOp.INSTANCE;
@@ -11,7 +9,6 @@ public final class ProgressIndicators {
 
 	public static void setInstance(
 			final ProgressIndicator instance) {
-		Logger.printError("4444 here");
 		ProgressIndicators.instance = instance;
 	}
 
